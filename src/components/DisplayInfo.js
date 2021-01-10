54f4b8a2915aafb31ab3890523a13d43cae0b35c
Milan CAMUS <pro@turtletv.fr>
2021-01-10 21:14:58 +0100

    Suppression fichiers inutiles

--- src/components/DisplayInfo.js
+++
@@ -1,9 +0,0 @@
-import '../assets/css/App.css';
-
-function DisplayInfo( {count, information} ) {
-    return(
-        <p className="displayInfos">{information}</p>
-    );
-}
-
-export default DisplayInfo;
